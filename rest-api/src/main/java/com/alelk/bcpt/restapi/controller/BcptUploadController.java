165dcc58e9b9b79ed8ed761cd18238026e6d6139
Alex Elkin <alelkdev@gmail.com>
2017-11-07 14:24:43 +0300

    Added functionality to download files.

--- rest-api/src/main/java/com/alelk/bcpt/restapi/controller/BcptUploadController.java
+++ rest-api/src/main/java/com/alelk/bcpt/restapi/controller/BcptUploadController.java
@@ -4,11 +4,15 @@ import com.alelk.bcpt.restapi.dto.FileInfoDto;
 import com.alelk.bcpt.storage.BcptStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,10 +42,22 @@ public class BcptUploadController {
         return ResponseEntity.ok(FileInfoDto.fromResource(storage.loadAsResource(fileName, category(category))));
     }
 
-    @GetMapping("/{category}/")
-    public ResponseEntity<List<FileInfoDto>> getDbfFileList(@PathVariable String category) {
+    @GetMapping("/{category}")
+    public ResponseEntity<List<FileInfoDto>> getFileList(@PathVariable String category) {
         return ResponseEntity.ok(storage.loadAllAsResources(category(category))
                 .map(FileInfoDto::fromResource).collect(Collectors.toList()));
+    }
+
+    @GetMapping("download/{category}")
+    public ResponseEntity<InputStreamResource> getFile(@PathVariable String category, @RequestParam String fileName) throws IOException {
+        log.info("Request to download {} file: '{}'", category, fileName);
+        Resource fileResource = storage.loadAsResource(fileName, category(category));
+        return ResponseEntity.ok()
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Pragma", "no-cache").header("Expires", "0")
+                .header("Content-Disposition", "attachment; filename=" + fileName)
+                .contentLength(fileResource.contentLength()).contentType(MediaType.TEXT_PLAIN)
+                .body(new InputStreamResource(fileResource.getInputStream()));
     }
 
     private String category(String externalCategory) {
