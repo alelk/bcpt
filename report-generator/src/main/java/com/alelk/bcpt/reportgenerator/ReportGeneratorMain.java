97561ef54f7151145a785a91a852d4eff1ac4a62
Alex Elkin <elckinne@gmail.com>
2017-11-22 16:08:30 +0300

    Changes in report: blood invoice series and blood invoice dates have been added.

--- report-generator/src/main/java/com/alelk/bcpt/reportgenerator/ReportGeneratorMain.java
+++ report-generator/src/main/java/com/alelk/bcpt/reportgenerator/ReportGeneratorMain.java
@@ -1,8 +1,8 @@
 package com.alelk.bcpt.reportgenerator;
 
-import com.alelk.bcpt.common.util.StringUtil;
 import com.alelk.bcpt.database.BcptDatabase;
 import com.alelk.bcpt.model.dto.BloodInvoiceDto;
+import com.alelk.bcpt.model.dto.BloodInvoiceSeriesDto;
 import com.alelk.bcpt.model.dto.BloodPoolDto;
 import com.alelk.bcpt.model.dto.ProductBatchDto;
 import com.alelk.bcpt.reportgenerator.datasource.BloodPoolsDataSource;
@@ -15,10 +15,8 @@ import net.sf.jasperreports.view.JasperViewer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -38,15 +36,18 @@ public class ReportGeneratorMain {
 
         BcptDatabase database = BcptDatabase.getInstance();
 
-        ProductBatchDto productBatchDto = database.getProductBatchService().findByExternalId("2017-50");
+        ProductBatchDto productBatchDto = database.getProductBatchService().findByExternalId("2017-1");
         log.info("Product batch: " + productBatchDto);
-        List<BloodPoolDto> bloodPools = database.getBloodPoolService().findByProductBatch("2017-50");
+        List<BloodPoolDto> bloodPools = database.getBloodPoolService().findByProductBatch("2017-1");
         log.info("Blood pools: {}", bloodPools);
-        List<BloodInvoiceDto> bloodInvoices = database.getBloodInvoiceService().findByProductBatch("2017-50");
+        List<BloodInvoiceDto> bloodInvoices = database.getBloodInvoiceService().findByProductBatch("2017-1");
         log.info("Blood invoices: {}", bloodInvoices.stream().map(BloodInvoiceDto::getExternalId).collect(Collectors.toList()));
+        List<BloodInvoiceSeriesDto> bloodInvoiceSeries = database.getBloodInvoiceSeriesService().findByProductBatch("2017-1");
+        log.info("Blood invoice series: {}", bloodInvoices.stream().map(BloodInvoiceDto::getExternalId).collect(Collectors.toList()));
 
         try {
-            JasperPrint print = JasperFillManager.fillReport(report, new LinkedHashMap<>(), new BloodPoolsDataSource(productBatchDto, bloodPools, bloodInvoices));
+            JasperPrint print = JasperFillManager.fillReport(report, new LinkedHashMap<>(),
+                    new BloodPoolsDataSource(productBatchDto, bloodPools, bloodInvoices, bloodInvoiceSeries));
             JasperViewer.viewReport(print);
         } catch (JRException e) {
             e.printStackTrace();
--- report-generator/src/main/java/com/alelk/bcpt/reportgenerator/datasource/BloodPoolsDataSource.java
+++ report-generator/src/main/java/com/alelk/bcpt/reportgenerator/datasource/BloodPoolsDataSource.java
@@ -1,15 +1,15 @@
 package com.alelk.bcpt.reportgenerator.datasource;
 
 import com.alelk.bcpt.model.dto.BloodInvoiceDto;
+import com.alelk.bcpt.model.dto.BloodInvoiceSeriesDto;
 import com.alelk.bcpt.model.dto.BloodPoolDto;
 import com.alelk.bcpt.model.dto.ProductBatchDto;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -22,20 +22,25 @@ import java.util.stream.Collectors;
 public class BloodPoolsDataSource implements JRDataSource {
 
     private final static Pattern BLOOD_DONATION_EXTERNAL_ID_PATTERN = Pattern.compile("^(\\d{6})(\\d{6})(\\d{2})$");
+    private final static SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd.MM.yy");
 
     private ProductBatchDto productBatchDto;
     private List<BloodPoolDto> bloodPools;
     private List<BloodInvoiceDto> bloodInvoices;
+    private List<BloodInvoiceSeriesDto> bloodInvoiceSeries;
     private int index = -1;
 
-    public BloodPoolsDataSource(ProductBatchDto productBatchDto, List<BloodPoolDto> bloodPools, List<BloodInvoiceDto> bloodInvoices) {
+    public BloodPoolsDataSource(ProductBatchDto productBatchDto, List<BloodPoolDto> bloodPools, List<BloodInvoiceDto> bloodInvoices, List<BloodInvoiceSeriesDto> bloodInvoiceSeries) {
         this.productBatchDto = productBatchDto;
         this.bloodPools = new ArrayList<>();
         this.bloodInvoices = new ArrayList<>();
+        this.bloodInvoiceSeries = new ArrayList<>();
         this.bloodPools.addAll(bloodPools);
         this.bloodInvoices.addAll(bloodInvoices);
+        this.bloodInvoiceSeries.addAll(bloodInvoiceSeries);
         this.bloodPools.sort(Comparator.comparingInt(BloodPoolDto::getPoolNumber));
         this.bloodInvoices.sort(Comparator.comparing(BloodInvoiceDto::getExternalId));
+        this.bloodInvoiceSeries.sort(Comparator.comparing(BloodInvoiceSeriesDto::getExternalId));
     }
 
     @Override
@@ -57,17 +62,34 @@ public class BloodPoolsDataSource implements JRDataSource {
             return bloodDonationIds.stream().map(bloodDonationId -> {
                 Matcher matcher = BLOOD_DONATION_EXTERNAL_ID_PATTERN.matcher(bloodDonationId);
                 if (!matcher.find()) return bloodDonationId;
-                return matcher.group(1) + "<style isBold='true' size='9'>" + matcher.group(2) + "</style>" + matcher.group(3);
+                return matcher.group(1) + "<style isBold='true' size='8'>" + matcher.group(2) + "</style>" + matcher.group(3);
             }).collect(Collectors.joining(" ")) + " ";
         }
+        final List<BloodInvoiceDto> bloodInvoices = this.bloodInvoices.stream()
+                .filter(bi -> bi.getBloodDonations() != null && bloodDonationIds.stream().anyMatch(bloodDonationId ->
+                        bi.getBloodDonations().contains(bloodDonationId)
+                )).collect(Collectors.toList());
         if ("bloodInvoiceIds".equals(fieldName)) {
-            return bloodInvoices.stream()
-                    .filter(bi ->
-                            bi.getBloodDonations() != null && bloodDonationIds.stream().anyMatch(bloodDonationId ->
-                                    bi.getBloodDonations().contains(bloodDonationId)
-                            )
-                    ).map(BloodInvoiceDto::getExternalId).collect(Collectors.joining(" "));
+            return bloodInvoices.stream().map(BloodInvoiceDto::getExternalId).collect(Collectors.joining(" "));
+        }
+        final List<BloodInvoiceSeriesDto> bloodInvoiceSeries = this.bloodInvoiceSeries.stream()
+                .filter(bis -> bis.getBloodInvoices() != null && bloodInvoices.stream().anyMatch(bloodInvoice ->
+                        bis.getBloodInvoices().contains(bloodInvoice.getExternalId())
+                )).collect(Collectors.toList());
+        if ("bloodInvoiceSeriesIds".equals(fieldName)) {
+            return bloodInvoiceSeries.stream().map(BloodInvoiceSeriesDto::getExternalId).collect(Collectors.joining(" "));
+        }
+        if ("bloodInvoiceDates".equals(fieldName)) {
+            List<Date> dates = bloodInvoiceSeries.stream().map(BloodInvoiceSeriesDto::getSeriesDate).collect(Collectors.toList());
+            if (dates.size() == 0)
+                dates = bloodInvoices.stream().map(BloodInvoiceDto::getDeliveryDate).collect(Collectors.toList());
+            return dates.stream().filter(Objects::nonNull).distinct().map(BloodPoolsDataSource::mapDateToSimpleString)
+                    .collect(Collectors.joining(" "));
         }
         return null;
+    }
+
+    private static String mapDateToSimpleString(Date date) {
+        return date == null ? null : SIMPLE_DATE_FORMAT.format(date);
     }
 }
